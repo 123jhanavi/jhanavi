603d64b363309d9b70e7b6b6d2e381cac2f234a0
deepu <jhanavi6633@gmail.com>
2023-09-24 18:34:13 +0530

    project2

---
+++ project2/src/main/java/group2/pages/Start.java
@@ -0,0 +1,14 @@
+package group2.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application project2.
+ */
+public class Start
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
